c7feb32414254ff50519ff7b232c96b725efb216
Carlos Bronze <170220+bronze@users.noreply.github.com>
2021-06-16 00:46:12 -0300

    viteSingleFile

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import { imagetools } from "vite-imagetools"
 import WindiCSS from 'vite-plugin-windicss'
 const typography = require('windicss/plugin/typography')
+import viteCompression from 'vite-plugin-compression'
+import { viteSingleFile } from "vite-plugin-singlefile"
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,6 +12,30 @@ export default defineConfig({
   },
   safelist: ['prose', 'prose-sm', 'm-auto'],
   darkMode: 'class',
-  plugins: [typography, WindiCSS(), imagetools()],
+  plugins: [
+    typography,
+    WindiCSS(),
+    imagetools(),
+    viteSingleFile(),
+    // gizp
+    viteCompression(),
+    // br
+    viteCompression({
+      ext: '.br',
+      algorithm: 'brotliCompress',
+    }),],
+    build: {
+      target: "esnext",
+      assetsInlineLimit: 100000000,
+      chunkSizeWarningLimit: 100000000,
+      cssCodeSplit: false,
+      brotliSize: false,
+      rollupOptions: {
+        inlineDynamicImports: true,
+        output: {
+          manualChunks: () => "everything.js",
+        },
+      },
+    },
   theme: {}
 })
