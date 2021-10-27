5a6d3bd1c59e7071802704091e53fa97a326cf1f
Carlos Bronze <170220+bronze@users.noreply.github.com>
2021-10-27 19:17:21 -0300

    analytics csp

--- public/analytics.js
+++ public/analytics.js
@@ -2,7 +2,4 @@ window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 
-gtag('config', 'UA-42538178-1', {
-cookie_domain: 'digitalcanvas.com.br',
-cookie_flags: 'SameSite=None;Secure',
-});
+gtag('config', 'UA-42538178-1');
