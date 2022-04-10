3b39a46869cbef52708d247b98efa08125a19044
Carlos Bronze <170220+bronze@users.noreply.github.com>
2022-04-09 22:27:23 -0300

    new quotes script

--- src/scripts/quotes.js
+++ src/scripts/quotes.js
@@ -1,41 +1,41 @@
-//Use an array to hold the value of the quotes
-const arrayOfQuotes = [{
-  "quoteText": "It's kind of fun to do the impossible",
-  "quoteAuthor": "Walt Disney"
-}, {
-  "quoteText": "All we have to decide is what to do with the time that is given to us",
-  "quoteAuthor": "J.R.R. Tolkien"
-}, {
-  "quoteText": "Imagination is the only weapon against reality",
-  "quoteAuthor": "Lewis Carroll"
-}, {
-  "quoteText": "You can't go back and change the beggining, but you can start where you are and change the ending",
-  "quoteAuthor": "C. S. Lewis"
-}, {
-  "quoteText": "The only way the magic works is by hard work. But hard wok can be fun",
-  "quoteAuthor": "Jim Hanson"
-}, {
-  "quoteText": "Someday you will be old enough to start reading fairy tales again",
-  "quoteAuthor": "C. S. Lewis"
-}, {
-  "quoteText": "If you don’t know where you’re going, any road will take you there",
-  "quoteAuthor": "Lewis Carroll"
-}];
+// https://blog.kritikapattalam.com/build-a-random-quote-generator-using-javascript
+const generateQuote = function () {
+  const quotes = [
+    {
+      quote: "It's kind of fun to do the impossible",
+      author: "Walt Disney"
+    },
+    {
+      quote: "All we have to decide is what to do with the time that is given to us",
+      author: "J.R.R. Tolkien"
+    },
+    {
+      quote: "Imagination is the only weapon against reality",
+      author: "Lewis Carroll"
+    },
+    {
+      quote: "You can't go back and change the beggining, but you can start where you are and change the ending",
+      author: "C. S. Lewis"
+    },
+    {
+      quote: "The only way the magic works is by hard work. But hard wok can be fun",
+      author: "Jim Hanson"
+    },
+    {
+      quote: "Someday you will be old enough to start reading fairy tales again",
+      author: "C. S. Lewis"
+    },
+    {
+      quote: "If you don’t know where you’re going, any road will take you there",
+      author: "Lewis Carroll"
+    }
+  ];
 
-function generateQuote() {
-  const random = Number.parseInt(Math.random() * arrayOfQuotes.length);
-  document.querySelector(
-    "#quote"
-  ).textContent = `${arrayOfQuotes[random].quoteText}`;
-  document.querySelector(
-    "#by"
-  ).textContent = `${arrayOfQuotes[random].quoteAuthor}`;
+  let arrayIndex = Math.floor(Math.random() * quotes.length);
+  document.getElementById("quote").innerHTML = quotes[arrayIndex].quote;
+  document.getElementById("author").innerHTML = quotes[arrayIndex].author;
+
 }
-function ready(fn) { // https://youmightnotneedjquery.com/#ready
-  if (document.readyState != 'loading') {
-    fn();
-  } else {
-    document.addEventListener('DOMContentLoaded', fn);
-  }
+window.onload = function () {
+  generateQuote();
 }
-ready(function () { generateQuote(); });
