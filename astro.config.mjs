db46d70e4c1b72c8d7a08b9948029c9616b701b3
Carlos Bronze <170220+bronze@users.noreply.github.com>
2022-05-01 22:22:13 -0300

    astro-image-tools 0.6.2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,18 @@
 import { defineConfig } from 'astro/config';
+import { astroImageTools } from "astro-imagetools";
 import sitemap from '@astrojs/sitemap';
-import astroImagePlugin from "astro-imagetools/plugin";
 import WindiCSS from 'vite-plugin-windicss'
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [astroImagePlugin, WindiCSS({
+    plugins: [WindiCSS({
       preflight: true
     })],
     ssr: {
       external: ["svgo"],
     },
   },
-  integrations: [sitemap()],
+  integrations: [astroImageTools, sitemap()],
   site: 'https://www.digitalcanvas.com.br/',
 });
