fe4109e8f324be00058c034ebb03c5d0db78bbcd
Carlos Bronze <170220+bronze@users.noreply.github.com>
2022-09-17 01:33:14 -0300

    upgrades people

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,18 @@
 import { defineConfig } from 'astro/config';
-import { astroImageTools } from "astro-imagetools";
+import UnoCSS from 'unocss/astro';
+import compress from "astro-compress";
+import critters from "astro-critters";
+import image from "@astrojs/image";
 import sitemap from '@astrojs/sitemap';
-import WindiCSS from 'vite-plugin-windicss'
+
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [WindiCSS({
-      preflight: true
-    })],
     ssr: {
-      external: ["svgo"],
-    },
+      external: ["svgo"]
+    }
   },
-  integrations: [astroImageTools, sitemap()],
-  site: 'https://www.digitalcanvas.com.br/',
+  integrations: [sitemap(), image(), UnoCSS(), compress(), critters()],
+  site: 'https://www.digitalcanvas.com.br/'
 });
