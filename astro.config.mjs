2f4057bc5a9eca232147f3a1b3524266660b3fee
Carlos Bronze <170220+bronze@users.noreply.github.com>
2024-06-08 12:09:31 -0300

    server

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,12 +8,12 @@ import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://www.digitalcanvas.com.br/',
   integrations: [icon(), tailwind({
     // Example: Disable injecting a basic `base.css` import on every page.
     // Useful if you need to define and/or import your own custom `base.css`.
     applyBaseStyles: true
   }), sitemap(), alpinejs()],
-  site: 'https://www.digitalcanvas.com.br/',
-  output: "static",
+  output: "server",
   adapter: netlify()
 });
