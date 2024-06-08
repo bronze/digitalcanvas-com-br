eac9111e77b2006d4cd15e5849a48f75b41b56f6
Carlos Bronze <170220+bronze@users.noreply.github.com>
2024-06-07 21:16:05 -0300

    jampack

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import compress from "astro-compress";
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
-
 import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
@@ -12,13 +11,7 @@ export default defineConfig({
     // Example: Disable injecting a basic `base.css` import on every page.
     // Useful if you need to define and/or import your own custom `base.css`.
     applyBaseStyles: true
-  }), sitemap(), compress({
-    // CSS: false,
-    HTML: true
-    // Image: false,
-    // JavaScript: false,
-    // SVG: false,
-  })],
+  }), sitemap()],
   site: 'https://www.digitalcanvas.com.br/',
   output: "server",
   adapter: netlify()
